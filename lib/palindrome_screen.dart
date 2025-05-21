ed64b8e8b8b67a3befbf19f91946c0f8ac6b51ad
Olga <olga@supports-MacBook-Pro.local>
2025-05-21 11:50:13 +0200

    Update palindrome_sreen.dart: Fix error in the class name

--- lib/palindrome_screen.dart
+++ lib/palindrome_screen.dart
@@ -2,15 +2,15 @@
 import 'package:flutter/material.dart';
 
 // Main UI widget for the palindrome checker
-class PalindromChecker extends StatefulWidget {
-  const PalindromChecker({super.key});
+class PalindromeChecker extends StatefulWidget {
+  const PalindromeChecker({super.key});
 
   @override
-  State<PalindromChecker> createState() => _PalindromCheckerState();
+  State<PalindromeChecker> createState() => _PalindromeCheckerState();
 }
 
 // State class for managing the UI
-class _PalindromCheckerState extends State<PalindromChecker> {
+class _PalindromeCheckerState extends State<PalindromeChecker> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
